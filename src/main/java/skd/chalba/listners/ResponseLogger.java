53d1d9feb4a0d918f3728ace9540ed9c4d2da7fd
sapandang <skdang1993@gmail.com>
2020-11-03 22:07:56 +0530

    updated the code

--- src/main/java/skd/chalba/listners/ResponseLogger.java
+++ src/main/java/skd/chalba/listners/ResponseLogger.java
@@ -18,16 +18,31 @@ public class ResponseLogger implements RequestsListners {
         if(!logOnce)
         {
             //print the headers
-            Logger.tag("RESPONSE").debug("ThreadName"+"|"+
-                    "ResponseCode"+"|"+
-                    "StatusMessage"+"|"+
-                    "RequestURL"+"|"+
-                    "RequestSentAtMillis"+"|"+
-                    "ResponseReceivedAtMillis"+"|"+
-                    "ResponseSendTime"+"|"+
-                    "ResponseReceivedTime"+"|"+
-                    "ResponseTimeAtMillis");
+//            Logger.tag("RESPONSE").debug("ThreadName"+"|"+
+//                    "ResponseCode"+"|"+
+//                    "StatusMessage"+"|"+
+//                    "RequestURL"+"|"+
+//                    "RequestSentAtMillis"+"|"+
+//                    "ResponseReceivedAtMillis"+"|"+
+//                    "ResponseSendTime"+"|"+
+//                    "ResponseReceivedTime"+"|"+
+//                    "ResponseTimeAtMillis");
             //header printed
+
+            Logger.tag("RESPONSE").debug("timeStamp," +
+                    "elapsed," +
+                    "label," +
+                    "responseCode," +
+                    "responseMessage," +
+                    "threadName," +
+                    "dataType," +
+                    "success," +
+                    "failureMessage," +
+                    "bytes,sentBytes,grpThreads,allThreads,Latency,IdleTime,Connect");
+
+            //new Headers
+
+
             logOnce=true;
         }
 
@@ -35,17 +50,58 @@ public class ResponseLogger implements RequestsListners {
 
         long startTime = System.currentTimeMillis();
 
-        Logger.tag("RESPONSE").debug(response.getThreadName()+"|"+
-                response.code+"|"+
-                response.message+"|"+
-                response.url+"|"+
-                response.sentRequestAtMillis+"|"+
-                response.receivedResponseAtMillis+"|"+
-                response.sendTimeStamp+"|"+
-                response.receivedTimeStamp+"|"+
-                response.responseTimeAtMillis);
+//        Logger.tag("RESPONSE").debug(response.getThreadName()+"|"+
+//                response.code+"|"+
+//                response.message+"|"+
+//                response.url+"|"+
+//                response.sentRequestAtMillis+"|"+
+//                response.receivedResponseAtMillis+"|"+
+//                response.sendTimeStamp+"|"+
+//                response.receivedTimeStamp+"|"+
+//                response.responseTimeAtMillis);
+
+        try{
+
+
+            String label = escapeSpecialCharacters(response.url.split("/")[(response.url.split("/").length - 1)]);
+            String success = "true";
+            String failureMessage = "";
+            if(response.code!=200)
+            {
+                success = "false";
+                failureMessage= escapeSpecialCharacters(response.body);
+            }
+            Logger.tag("RESPONSE").debug(response.receivedResponseAtMillis+","
+                    +response.responseTimeAtMillis+","
+                    +label+","+
+                    response.code+","+
+                    response.message+","+
+                    response.getThreadName()+","+
+                    "text,"+
+                    success+","+
+                    failureMessage+",0,0,0,0,"+
+                    response.responseTimeAtMillis+",0,0"
+                    );
+
+
+
+        }catch (Exception e)
+        {
+            Logger.error("logger error");
+        }
+
 
         //System.out.println("Time Taken to write in file -> "+(System.currentTimeMillis()-startTime ));
 
+    }
+
+
+    public static String escapeSpecialCharacters(String data) {
+        String escapedData = data.replaceAll("\\R", " ");
+        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+            data = data.replace("\"", "\"\"");
+            escapedData = "\"" + data + "\"";
+        }
+        return escapedData;
     }
 }
